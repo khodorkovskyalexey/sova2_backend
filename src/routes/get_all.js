73c2785b682d810d3395e8502bdbd615471bb600
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-11-11 16:58:15 +0600

    add get_all endpoints for development

---
+++ src/routes/get_all.js
@@ -0,0 +1,18 @@
+const router = require('koa-router')()
+const { User, Test, Question, Answer } = require('../database/db')
+
+router
+    .get('/users',async ctx => {
+        ctx.body = await User.findAll()
+    })
+    .get('/tests', async ctx => {
+        ctx.body = await Test.findAll()
+    })
+    .get('/questions', async ctx => {
+        ctx.body = await Question.findAll()
+    })
+    .get('/answers', async ctx => {
+        ctx.body = await Answer.findAll()
+    })
+
+module.exports = router
