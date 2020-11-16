cc784016068bc39b87da80c6ca7fe6beda2d71f4
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-11-16 16:57:32 +0600

    add authentification in last commit

--- src/middlewares/find_test.js
+++ src/middlewares/find_test.js
@@ -1,8 +1,7 @@
-const { Test } = require('../database/db')
-
 module.exports = async (ctx, next) => {
-    ctx.request.body["test"] = await Test.findOne({ where:{ test_id: ctx.params["test_id"] } })
-    if(ctx.request.body["test"] == null) {
+    ctx.request.body["test"] = (await ctx.request.body["author"].getTests({
+        where:{ test_id: ctx.params["test_id"] } }))[0]
+    if(ctx.request.body["test"] === undefined) {
         throw new Error('Test not found by test_id')
     }
     await next()
--- src/routes/teacher_result.js
+++ src/routes/teacher_result.js
@@ -1,10 +1,11 @@
 const router = require('koa-router')()
 
 //middlewares
+const author_auth = require('../middlewares/find_tests_author_by_token')
 const find_test = require('../middlewares/find_test')
 
 router
-    .get('/:token/result/:test_id', find_test, async ctx => {
+    .get('/:token/result/:test_id', author_auth, find_test, async ctx => {
         const student_results = await ctx.request.body["test"].getResults({
             attributes: ["fio", "group", "mark"] })
         const res = []
