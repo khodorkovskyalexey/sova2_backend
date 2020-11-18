5e5ff6f23b1b82dfbe3f0353ab5d3913235b85dd
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-11-18 17:09:34 +0600

    fix logic bug

--- src/middlewares/find_test.js
+++ src/middlewares/find_test.js
@@ -1,6 +1,7 @@
+const { Test } = require('../database/db')
 module.exports = async (ctx, next) => {
-    ctx.request.body["test"] = (await ctx.request.body["author"].getTests({
-        where:{ test_id: ctx.params["test_id"] } }))[0]
+    ctx.request.body["test"] = await Test.findOne({
+        where: { test_id: ctx.params["test_id"] } })
     if(ctx.request.body["test"] === undefined) {
         throw new Error('Test not found by test_id')
     }
