d4728147aca824b630d01a634bdaecd4d0ac4f00
Alexey Khodorkovsky <aezz120813@gmail.com>
2020-11-24 16:51:24 +0600

    add procfile for heroku

--- prod.sh
+++ prod.sh
@@ -5,5 +5,5 @@ set -e
 export DB_CONNECTION=$(echo $DB_CONNECTION)
 export NODE_ENV=production
 
-docker build -t sova .
-docker run -p 49160:8081 -d sova
+docker build -t sova2 .
+docker run -p 49160:8081 -d sova2
